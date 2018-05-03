fafa67b0d09894424ef29bddd439e6d46473b47c
Recursif <jillian.dum@gmail.com>
2018-05-03 08:47:20 +0200

    encode finish

--- address-encode/address.js
+++ address-encode/address.js
@@ -1,6 +1,9 @@
 var x = "0xcD1195E94b293Da440a732F4361D451d3B439862";
 
 
+function corner(i, j, size) {
+  return ((i == 0 && j == 0) || (i == size - 1 && j == 0) || (i == 0 && j == size - 1) || (i == size - 1 && j == size - 1));
+}
 
 function setup() {
   var res = 20;
@@ -11,22 +14,33 @@ function setup() {
 
   var color = '#efefef';
   var n = 2;
-  var p = 2;
+  var p = 0;
   var binary;
-  var code = x.charAt(n);
+  var code = x.charCodeAt(n);
+  console.log(x.charAt(n));
   for (var j = 0; j < size; j++) {
     for (var i = 0; i < size; i++) {
-      binary = code % 2;
-      code = (code) ? Math.ceil(code / 2) : 0;
+      if (corner(i, j, size))
+      {
+        console.log("pass");
+        fill('#efefef');
+        rect(i * res , j * res, i * res + res ,  j * res + res)
+        continue;
+      }
       console.log(code);
-      color = (binary) ? '#efefef' : '#0c1f3c';
+      binary = code % 2;
+      color = (binary) ? '#0c1f3c' : '#efefef';
       fill(color);
       rect(i * res , j * res, i * res + res ,  j * res + res);
-      p *= 2;
-      if (p == 128) {
+      code = (code == 0) ? 0 : Math.floor(code / 2);
+      p++;
+      if (p == 8) {
         n++;
-        code = x.charAt(n)
-        p = 2;
+        code = x.charCodeAt(n);
+
+        console.log("next");
+        console.log(x.charAt(n));
+        p = 0;
       }
     }
   }
