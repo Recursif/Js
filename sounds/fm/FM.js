515dbf689fe7b5f7da72d8673d81f2d267b1345a
Recursif <jillian.dum@gmail.com>
2018-07-06 17:08:50 +0200

    wtf

--- sounds/fm/FM.js
+++ sounds/fm/FM.js
@@ -9,19 +9,19 @@ function setup() {
 
   textPorteuse = createP('Fréquence de la porteuse');
   textPorteuse.position(10, 10)
-  porteuse = createSlider(0, 360);
+  porteuse = createSlider(220, 440);
   porteuse.position(10, 60)
   porteuse.style('width', '180px');
 
   textModulante = createP('Fréquence de la modulante');
   textModulante.position(10, 110)
-  modulante = createSlider(0, 360);
+  modulante = createSlider(440, 1280);
   modulante.position(10, 160)
   modulante.style('width', '180px');
 
   textModulation = createP('Indice de modulation');
   textModulation.position(10, 210)
-  indiceModulation = createSlider(0, 360);
+  indiceModulation = createSlider(0, 50);
   indiceModulation.position(10, 260)
   indiceModulation.style('width', '180px');
   
@@ -31,15 +31,16 @@ function draw() {
   background(255, 100, 100);
 
   var fp = porteuse.value();
-  var valM = modulante.value();
-  var valI = indiceModulation.value();
+  var fm = modulante.value();
+  var Im = indiceModulation.value();
 
   stroke(0)
-  var wp = 2*PI*fp 
+  var wp = 2*PI*fp
+  var wm = 2*PI*fm 
   var t = 0;
   var inc = 4/(220 * 400);
   for (var i = 0; i < 400; i++) {
-    line(i, 200  + 150 * sin(wp*t) , i + 1 , 200 + 150 * sin(wp*(t + inc)));
+    line(i, 200  + 150 * sin(wp*t + Im*sin(wm*t)) , i + 1 , 200 + 150 * sin(wp*(t + inc) + Im*sin(wm*t + inc)));
     t = t + inc;
   }
 
