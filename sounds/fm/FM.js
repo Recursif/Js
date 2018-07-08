3f584d0b0e184226dd42c616d072af65cd582a69
Recursif <jillian.dum@gmail.com>
2018-07-08 08:04:33 +0200

    it's funny basic fm modulation signal representation

--- sounds/fm/FM.js
+++ sounds/fm/FM.js
@@ -1,27 +1,34 @@
 
 var porteuse, modulante, indiceModulation;
 var textPorteuse, textModulante, textModulation;
+var osc;
+
 
 function setup() {
   var canvas = createCanvas(400, 400);
   canvas.parent('sketch-holder');
 
+  osc = new p5.Oscillator();
+  osc.setType('sine');
+  osc.freq(240);
+  osc.amp(0);
+  osc.start();
 
   textPorteuse = createP('Fréquence de la porteuse');
   textPorteuse.position(10, 10)
-  porteuse = createSlider(220, 440);
+  porteuse = createSlider(220, 880);
   porteuse.position(10, 60)
   porteuse.style('width', '180px');
 
   textModulante = createP('Fréquence de la modulante');
   textModulante.position(10, 110)
-  modulante = createSlider(440, 1280);
+  modulante = createSlider(2200, 8000);
   modulante.position(10, 160)
   modulante.style('width', '180px');
 
   textModulation = createP('Indice de modulation');
   textModulation.position(10, 210)
-  indiceModulation = createSlider(0, 50);
+  indiceModulation = createSlider(0, 5, 4);
   indiceModulation.position(10, 260)
   indiceModulation.style('width', '180px');
   
@@ -38,9 +45,11 @@ function draw() {
   var wp = 2*PI*fp
   var wm = 2*PI*fm 
   var t = 0;
-  var inc = 4/(220 * 400);
+  var inc = 4/(440 * 400);
   for (var i = 0; i < 400; i++) {
-    line(i, 200  + 150 * sin(wp*t + Im*sin(wm*t)) , i + 1 , 200 + 150 * sin(wp*(t + inc) + Im*sin(wm*t + inc)));
+    line(i, 200  + 150 * sin(wp*t + Im*sin(wm*t)) , i + 1 , 200 + 150 * sin(wp*(t + inc) + Im*sin(wm*(t + inc) ) ));
+    osc.freq(240);
+
     t = t + inc;
   }
 
